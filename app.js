7b4f0c48e47341095e46dc19ad297c23be487fa5
rxlxndx <20223tn027@utez.edu.mx>
2025-11-26 22:25:22 -0600

    rutas cambiadas

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ btnDetener.addEventListener("click", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/geolocalizacion.github.io/sw.js")
       .then((reg) => console.log("Service Worker registrado:", reg.scope))
       .catch((err) => console.error("Error al registrar SW:", err));
   });
